26d3c9a691793c4e3e27f56f111278645c62444b
kotsiuk dmitro <kotsiukdmitro@gmail.com>
2024-08-15 14:01:13 +0300

    step 13-15

--- step9-15/AssignmentList.js
+++ step9-15/AssignmentList.js
@@ -1,15 +1,18 @@
 import Assignment from "./Assignment.js"
 import AssignmentTags from "./AssignmentTags.js"
+import Panel from "./Panel.js"
 
 export default {
-  components: { Assignment, AssignmentTags },
+  components: { Assignment, AssignmentTags, Panel },
   template: `
-    <section v-show="assignments.length" class="mb-5">
+    <Panel v-show="assignments.length" class="mb-5 w-62 " id="my-panel">
+    <div class="flex justify-between items-start">
       <h2 class="font-bold mb-2">
            {{title}}
            <span>({{assignments.length}})</span>
       </h2>
-     
+      <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+     </div>
           <assignment-tags
               v-model:currentTag="currentTag"
               :initial-tags="assignments.map(a => a.tag)"
@@ -22,16 +25,22 @@ export default {
            :assignment="assignment"
          ></assignment>
       </ul>
-  </section> 
+      <slot></slot>
+      <template #footer>
+        my footer
+      </template>
+  </Panel> 
 `,
   props: {
     assignments: Array,
-    title: String
+    title: String,
+    canToggle: {type: Boolean, default: false}
   },
 
   data() {
     return {
-      currentTag: 'all'
+      currentTag: 'all',
+      show: true
     }
 
   },
